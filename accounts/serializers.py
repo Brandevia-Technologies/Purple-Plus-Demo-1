1519f220ba8e5d5167c4426f1a05e10a6bd093f0
notjustsomesmalltowngirl <olukayodepeace2006@gmail.com>
2025-11-06 12:12:10 +0100

    passing group to be returned in serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -47,21 +47,42 @@ class CustomUserSerializer(serializers.ModelSerializer):
     patient_profile = PatientProfileSerializer(required=False)
     staff_profile = StaffProfileSerializer(required=False)
 
+    group = serializers.SerializerMethodField()
+
+
     class Meta:
         model = CustomUser
         fields = ['first_name','middle_name', 'last_name','sex',
                   'email', 'password', 'date_joined',
-                  'last_login','patient_profile', 'staff_profile']
+                  'last_login','patient_profile', 'staff_profile', 'group']
         extra_kwargs = {
-            'password': {'write_only': True},
+            'password': {'write_only': True},  # never return password in responses
             'date_joined': {'read_only': True},
             'last_login': {'read_only': True},
+            'is_staff': {'read_only': True},  # set by views/perform_create
+            'is_superuser': {'read_only': True},
+            'is_active': {'read_only': True},
         }
 
-    def create(self, validated_data):
+    def get_groups(self, obj):
+        group = obj.groups.first()
+        return group.name if group else None
 
+    def create(self, validated_data):
+        """
+        Create a CustomUser and attach related profile and group.
+        DRF will pass extra kwargs from serializer.save(...) into this method.
+        - validated_data: data validated by serializer (excludes nested profile dicts)
+        - group (optional): group name to add the user to (string)
+        - department (optional): department for staff_profile (string)
+        - created_by (optional): user who performed creation (useful for StaffProfile.created_by)
+        """
+        # 1) Remove nested profile data from validated_data so we can call create_user()
         patient_profile_data = validated_data.pop('patient_profile', None)
         staff_profile_data = validated_data.pop('staff_profile', None)
+
+        # 2) Create the user using manager's create_user (hashes password, sets defaults)
+        #    This expects fields like email, password, first_name etc in validated_data.
         user = CustomUser.objects.create_user(**validated_data)
         if patient_profile_data:
             PatientProfile.objects.filter(user=user).update(**patient_profile_data)
