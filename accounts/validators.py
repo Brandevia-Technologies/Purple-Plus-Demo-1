5e7b7c40308c797550b700d00dc2d3c2110d3cd7
notjustsomesmalltowngirl <olukayodepeace2006@gmail.com>
2025-11-12 14:37:57 +0100

    update validators

--- accounts/validators.py
+++ accounts/validators.py
@@ -1,6 +1,6 @@
 from django.core.validators import RegexValidator
 
 class NINValidator(RegexValidator):
-    regex = r'^\d{11}$'  # Example: Three uppercase letters followed by four digits
+    regex = r'^\d{11}$'
     message = 'Invalid NIN format. Must be 11 digits.'
     code = 'invalid_code_format'
