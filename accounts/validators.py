d78aa8d224cc0f43c38feeb87db0987f85c660fb
notjustsomesmalltowngirl <olukayodepeace2006@gmail.com>
2025-11-12 14:35:06 +0100

    remove accounts

--- accounts/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.validators import RegexValidator
-
-class NINValidator(RegexValidator):
-    regex = r'^\d{11}$'  # Example: Three uppercase letters followed by four digits
-    message = 'Invalid NIN format. Must be 11 digits.'
-    code = 'invalid_code_format'
