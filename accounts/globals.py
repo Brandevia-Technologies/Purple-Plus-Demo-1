81707785528d985c9daca52a7c86b7884b01b5f8
notjustsomesmalltowngirl <olukayodepeace2006@gmail.com>
2025-11-12 17:30:53 +0100

    add manage.py

--- accounts/globals.py
+++
@@ -1,6 +0,0 @@
-ALL_GROUPS = [
-        'Doctor', 'Clinician', 'Patient',
-        'Registrar', 'Outreach Coordinator', 'Outreach Worker',
-        'Public Health Analyst', 'Inventory Manager',
-        'Finance Officer', 'Cashier', 'Hr', 'Receptionist', 'Nurse'
-    ]
--- accounts/tests.py
+++
@@ -1,4 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-#  test to see if the permissions work correctly with grouping
