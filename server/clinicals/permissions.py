708369f538b0a192f647bd95044c149443f9c4fb
notjustsomesmalltowngirl <olukayodepeace2006@gmail.com>
2025-11-17 21:00:49 +0100

    update permissions

--- server/clinicals/permissions.py
+++ server/clinicals/permissions.py
@@ -6,10 +6,16 @@ class CanCreatePatientRecord(BasePermission):
                 request.user.has_perm('clinicals.can_create_patient_record')
                 and request.user.is_active)
 class CanViewPatientRecord(BasePermission):
-    ...
+    def has_permission(self, request, view):
+        return (request.user and request.user.is_staff and
+                request.user.has_perm('clinicals.can_view_patient_record'))
 
 class CanUpdatePatientRecord(BasePermission):
-    ...
+    def has_permission(self, request, view):
+        return (request.user and request.user.is_staff and
+                request.user.has_perm('clinicals.can_update_patient_record'))
 
 class CanDeletePatientRecord(BasePermission):
-    ...
+    def has_permission(self, request, view):
+        return (request.user and request.user.is_staff and
+                request.user.has_perm('clinicals.can_delete_patient_record'))
