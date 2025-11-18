da5ddfeb0bae96e46816f04384da8b85c6bd8c36
notjustsomesmalltowngirl <olukayodepeace2006@gmail.com>
2025-11-18 22:18:11 +0100

    add view for update and view

--- server/clinicals/urls.py
+++ server/clinicals/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreatePatientRecordView
+from .views import CreatePatientRecordView, ViewPatientRecords, UpdatePatientRecordView
 
 urlpatterns = [
     path(
@@ -7,4 +7,14 @@ urlpatterns = [
         CreatePatientRecordView.as_view(),
         name="create-patient-record"
     ),
+    path(
+        "patients/<int:patient_id>/records/view/",
+        ViewPatientRecords.as_view(),
+        name="view-patient-record"
+    ),
+    path(
+        "patients/<int:patient_id>/records/update/",
+        UpdatePatientRecordView.as_view(),
+        name="update-patient-record"
+    )
 ]
--- server/clinicals/views.py
+++ server/clinicals/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from .models import PatientRecord
 from accounts.models import PatientProfile
 from .serializers import PatientRecordSerializer
-from .permissions import CanViewPatientRecord, CanCreatePatientRecord
+from .permissions import CanViewPatientRecord, CanCreatePatientRecord, CanUpdatePatientRecord,  CanDeletePatientRecord
 
 class CreatePatientRecordView(generics.CreateAPIView):
     serializer_class = PatientRecordSerializer
@@ -13,4 +13,20 @@ class CreatePatientRecordView(generics.CreateAPIView):
     def perform_create(self, serializer):
         patient = get_object_or_404(PatientProfile, user__id=self.kwargs['patient_id'])
         serializer.save(patient=patient.user, created_by=self.request.user)
+
+
+class ViewPatientRecords(generics.ListAPIView):
+    serializer_class = PatientRecordSerializer
+    permission_classes = [CanViewPatientRecord]
+
+    def get_queryset(self):
+        patient_id = self.kwargs['patient_id']
+        # patient = PatientProfile.objects.filter()
+        return PatientRecord.objects.filter(patient=patient_id)
+
+
+class UpdatePatientRecordView(generics.UpdateAPIView):
+    permission_classes = [CanUpdatePatientRecord]
+    queryset = PatientRecord.objects.all()
+    serializer_class = PatientRecordSerializer
 
